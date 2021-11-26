7262ad92ae4bc7929a1ef9658f69ede9f6990e91
kyleteeter <kyleteeter12@gmail.com>
2021-11-26 12:49:41 -0600

    Can now update parents and students seperately and save and update their info in localStorage.

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function displayStudent(student) {
   let parents = [];
   Object.keys(student).forEach((key) => {
     if (key === "parents") {
-      parents = displayParent(student[key]);
+      parents = displayParent(student[key], student.guid);
     }
   });
   this.root.innerHTML += renderSingleStudent(student, parents);
@@ -29,54 +29,58 @@ function renderSingleStudent(student, parents) {
   return `<details class="student-card" id="${student.guid}"><summary><span class="student name" data-type="name">${student.name}</span></summary><p>Email: <span class="student email" data-type="email">${student.email}</span></p><p>Section: <span class="student section" data-type="section">${student.section}</span></p><p>Grade: <span class="student grade" data-type="grade">${student.grade}</span></p><button onclick="makeEditable('${student.guid}', 'student')">Edit</button><h3>${parents.length ? "Parents" : "No parents registered"}</h3>${parents}</details>`;
 }
 
-function displayParent(parents) {
+function displayParent(parents, studentID) {
   let results = [];
   if (!parents) {
     return [];
   } else if (parents.constructor.name === "Array") {
     parents.forEach((parent) => {
-      //localStorage.setItem(parent.guid, JSON.stringify(parent));
-      results += renderSingleParent(parent); 
+      results += renderSingleParent(parent, studentID);
     });
   } else {
-    //localStorage.setItem(parents.guid, JSON.stringify(parents));
-    results += renderSingleParent(parents);
+    results += renderSingleParent(parents, studentID);
   }
   return results;
 }
 
-function renderSingleParent(parent) {
-  return `<details class="parent-card" id="${parent.guid}"><summary><span class="parent name" data-type="name">${parent.name}</span></summary><p>Email: <span class="parent email" data-type="email">${parent.email}</span></p><p>Phone: <span class="parent phone" data-type="phone">${parent.phone}</span></p> <button onclick="makeEditable('${parent.guid}', 'parent')">Edit</button></details>`;
-}
-
-function makeEditable(guid, cardClass) {
-  let editBtn = document.getElementById(guid).querySelector("button");
-  let editables = document.getElementById(guid).getElementsByClassName(cardClass);
-  for (var i = 0; i < localStorage.length; i++) {
-    Object.keys(editables).forEach((key) => {
-      if (!editables[key].isContentEditable) {
-        editables[key].contentEditable = "true";
-        editables[key].addEventListener(
-          "input",
-          function () {
-            let person = JSON.parse(localStorage.getItem(guid));
-            let keyName = editables[key].getAttribute("data-type");
-            person[keyName] = editables[key].innerText;
-            localStorage.setItem(guid, JSON.stringify(person));
-          },
-          false
-        );
-        editBtn.innerHTML = "Done Editing";
-      } else {
-        editables[key].contentEditable = "false";
-        editBtn.innerHTML = "Edit";
-      }
-    });
+function renderSingleParent(parent, studentID) {
+  let editedParent = localStorage.getItem(parent.guid);
+  if (editedParent) {
+    parent = JSON.parse(editedParent);
   }
-  // console.log("student", student);
-  // saveChanges(guid, student)
+  return `<details class="parent-card" id="${parent.guid}"><summary><span class="parent name" data-type="name">${parent.name}</span></summary><p>Email: <span class="parent email" data-type="email">${parent.email}</span></p><p>Phone: <span class="parent phone" data-type="phone">${parent.phone}</span></p> <button onclick="makeEditable('${parent.guid}', 'parent', '${studentID}')">Edit</button></details>`;
 }
 
-function saveChanges(guid, student) {
-  localStorage.setItem(guid, student);
+function makeEditable(guid, cardClass, studentID) {
+  let editBtn = document.getElementById(guid).querySelector("button");
+  let editables = document
+    .getElementById(guid)
+    .getElementsByClassName(cardClass);
+  Object.keys(editables).forEach((key) => {
+    if (!editables[key].isContentEditable) {
+      editables[key].contentEditable = "true";
+      editables[key].addEventListener(
+        "input",
+        function () {
+          let person = JSON.parse(localStorage.getItem(guid));
+          if (person == null && studentID) {
+            person = findParent(studentID, guid);
+          }
+          let keyName = editables[key].getAttribute("data-type");
+          person[keyName] = editables[key].innerText;
+          localStorage.setItem(guid, JSON.stringify(person));
+        },
+        false
+      );
+      editBtn.innerHTML = "Done Editing";
+    } else {
+      editables[key].contentEditable = "false";
+      editBtn.innerHTML = "Edit";
+    }
+  });
+}
+
+function findParent(studentID, parentGuid) {
+  let student = JSON.parse(localStorage.getItem(studentID));
+  return student.parents.find((parent) => parent.guid === parentGuid);
 }
